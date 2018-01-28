7609f2487649af5c6b5ee18bd404eade857874d6
unleashalicia <alicia.adams.evans@gmail.com>
2018-01-28 13:06:02 -0800

    First components and assets created

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+import Game from './game';
 
 const App = () => (
     <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
+        <Game/>
     </div>
 );
 
