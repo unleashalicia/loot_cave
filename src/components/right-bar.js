b5f1ae205120b41410dff9c2b7e898cdd288ab30
unleashalicia <alicia.adams.evans@gmail.com>
2018-02-06 17:08:08 -0800

    Basic sections added for right menu

--- src/components/right-bar.js
+++ src/components/right-bar.js
@@ -4,9 +4,28 @@ import '../assets/css/right-bar.css';
 class RightBar extends Component{
     render(){
         return (
-            <div id="right-bar">
-                <p>TBD STUFF</p>
-            </div>
+            <section id="right-bar" className ="right-menu">
+                <div className="stats-container">
+                    <div className="title-div">
+                        <h3>Memory Stats</h3>
+                    </div>
+                    <div className="games-played">
+                        <p className="label">Total Games : </p>
+                        <div className="value"></div>
+                    </div>
+                    <div className="attempts">
+                        <p className="label">Attempts : </p>
+                        <div className="value"></div>
+                    </div>
+                    <div className="accuracy">
+                        <p className="label">Accuracy : </p>
+                        <div className="value"></div>
+                    </div>
+                    <button className="reset">New Game</button>
+                    <i className="fa fa-music" aria-hidden="true"></i>
+                    <button className="audio"><i className="fa fa-play"></i></button>
+                </div>
+            </section>
         )
     }
 }
