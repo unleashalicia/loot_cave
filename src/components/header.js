4a8710fea4cfd0a50d6e253c142e1131e92808c3
unleashalicia <alicia.adams.evans@gmail.com>
2018-02-06 19:58:50 -0800

    Header styling complete.  Link not pathed yet

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import logo from '../assets/images/dragon_logo.png';
 import '../assets/css/header.css';
 
 export default () => {
     return (
         <div id="header">
-            <h1>Loot Cave</h1>
+            <div>
+                <div>
+                    <img src={logo} alt="dragon logo"/>
+                </div>
+            </div>
+            <div>
+                <h1>Loot Cave</h1>
+            </div>
+            <div>
+                <h3 className="making-of-link"><a href="#" target="_blank">Instructions</a></h3>
+            </div>
         </div>
     )
 }
